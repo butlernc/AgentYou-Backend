ed0c779e03afa81ac0f2cf31a97ad17ae8fddf04
Noah <butlernc@uwec.edu>
2015-06-25 12:53:00 -0700

    fixed agents.views.create

--- Agents/views.py
+++ Agents/views.py
@@ -4,9 +4,9 @@ from Agents import models
 # Creates a new Agent, save's their email and password
 # along with the face data
 def create(request):
-    email = request.POST["email"]
-    password = request.POST["password"]
-    id_pic_address = request.POST["id_pic_address"]
+    email = request.POST['email']
+    password = request.POST['password']
+    id_pic_address = ''
 
     new_agent = models.AgentUser(email=email, password=password)
     new_agent.save()
@@ -14,7 +14,7 @@ def create(request):
     # id_pic_address goes to the C++ file
     # returns data to save in FaceData.DATA_0
 
-    data = ""
+    data = "1"
     new_agent_face_data = models.FaceData(email=email, DATA_0=data)
     new_agent_face_data.save()
     response = ""
