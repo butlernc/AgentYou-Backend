7a6b050a338e7bd8d0156ea4dac5c579e52233e6
butlernc <butlernc@uwec.edu>
2015-06-25 11:15:42 -0400

    settings config'd for server

--- AgentYou/settings.py
+++ AgentYou/settings.py
@@ -59,11 +59,12 @@ WSGI_APPLICATION = 'AgentYou.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'AGENTYOU_DATABASE',
-        'USER': 'root',
-        'PASSWORD': '123ncb',
-        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'W91f6GsCba',
+        'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
