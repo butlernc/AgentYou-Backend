16c5bc3f9c7a1302a280c127853aacf820cef79b
Noah <butlernc@uwec.edu>
2015-07-09 06:27:36 -0700

    switch from post to get

--- AgentYou/settings.py
+++ AgentYou/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SETTINGS_DIR = os.path.dirname(__file__)
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -47,6 +51,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
 )
 
 ROOT_URLCONF = 'AgentYou.urls'
@@ -86,3 +94,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
--- Agents/views.py
+++ Agents/views.py
@@ -1,24 +1,28 @@
-from django.http import HttpResponse
+
 from Agents import models
 from django.template import RequestContext
+from django.shortcuts import render_to_response
 # Creates a new Agent, save's their email and password
 # along with the face data
 
 def create(request):
-    post_content = RequestContext(request)
-    email = post_content['email']
-    password = post_content['password']
-    id_pic_address = ''
 
-    new_agent = models.AgentUser(email=email, password=password)
-    new_agent.save()
+    context = RequestContext(request)
 
-    # id_pic_address goes to the C++ file
-    # returns data to save in FaceData.DATA_0
+    if request.method == 'GET':
+        email = context['email']
+        password = context['password']
+        id_pic_address = ''
 
-    data = "1"
-    new_agent_face_data = models.FaceData(email=email, DATA_0=data)
-    new_agent_face_data.save()
-    response = "what upppp niggaaa"
+        new_agent = models.AgentUser(email=email, password=password)
+        new_agent.save()
 
-    return HttpResponse(response)
+        # id_pic_address goes to the C++ file
+        # returns data to save in FaceData.DATA_0
+
+        data = "1"
+        new_agent_face_data = models.FaceData(email=email, DATA_0=data)
+        new_agent_face_data.save()
+
+        context_dict = {'worked': True}
+    return render_to_response(create_user.html, context_dict, context)
